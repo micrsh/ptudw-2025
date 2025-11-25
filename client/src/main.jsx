e6a9dd9e59a3222e0bd936feb698d6e97bb49541
Micrsh <Meling240399@gmail.com>
2025-11-25 23:20:54 +0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={import.meta.env.VITE_API_URL}>
+  <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
