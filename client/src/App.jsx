6d8247a7f1efb6cb30ac2e92244d7c193c49b792
Micrsh <Meling240399@gmail.com>
2025-11-28 21:17:09 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,16 @@ import RoutesDef from "./RoutesDef";
 
 function App() {
   useEffect(() => {
-    // // nap main.js sau khi react da tao Virtual DOM
-    // const script = document.createElement("script");
-    // script.src = "/js/main.js";
-    // script.async = false; // giu thu tu thuc thi cua file
-    // document.body.appendChild(script);
-    // return () => {
-    //   // don dep script khi unmount
-    //   document.body.removeChild(script);
-    // };
-  }, []);
+    // nap main.js sau khi react da tao Virtual DOM
+    const script = document.createElement("script");
+    script.src = "/js/main.js";
+    script.async = false; // giu thu tu thuc thi cua file
+    document.body.appendChild(script);
+    return () => {
+      // don dep script khi unmount
+      document.body.removeChild(script);
+    };
+  });
 
   return (
     <Layout>
